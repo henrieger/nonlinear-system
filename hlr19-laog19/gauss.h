2119b538d77358b550f44bd66095a585e0b28e40
Henrique Rieger <henriquerieger2001@gmail.com>
2021-11-11 19:36:07 -0300

    [fix] - cabeçalho da função gauss

--- hlr19-laog19/gauss.h
+++ hlr19-laog19/gauss.h
@@ -2,5 +2,4 @@
 /* Leon Augusto Okida Gonçalves - GRR20190365 - laog19 */
 
 /* Calcula o sistema linear pelo método de Gauss com pivoteamento parcial */
-/* Os termos independentes são sempre 0 */
-int gauss(int dimensao, double **a, double *x);
+void gauss(int dimensao, double **a, double *b, double *x);
