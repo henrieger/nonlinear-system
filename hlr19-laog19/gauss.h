9d5bd39d03d75afa66671efb2490e31f226af5c4
Henrique Rieger <henriquerieger2001@gmail.com>
2021-11-12 11:10:01 -0300

    [add] Informações sobre enum de retorno em gauss.h

--- hlr19-laog19/gauss.h
+++ hlr19-laog19/gauss.h
@@ -4,7 +4,12 @@
 #ifndef __GAUSS__
 #define __GAUSS__
 
-enum t_sistemas {SPD, SPI, SI, MAX_IT};
+enum t_sistemas {
+    SPD, /* Sistema Possível e Determinado */
+    SPI, /* Sistema Possível e Indeterminado */
+    SI, /* Sistema Impossível */
+    MAX_IT /* Limite de iterações no cálculo atingido */
+};
 
 /* Calcula o sistema linear pelo método de Gauss com pivoteamento parcial */
 enum t_sistemas gauss(int n, double **a, double *b, double *x);
