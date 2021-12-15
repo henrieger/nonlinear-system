72cf48923e93a9a5ffdd5e85de73b2ddaa8f0ce0
Henrique Rieger <henriquerieger2001@gmail.com>
2021-12-15 20:15:38 -0300

    [fix] - parenteses da macro de PAD

--- hlr19-laog19/utils.h
+++ hlr19-laog19/utils.h
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 
 /* Padding para evitar vetores com tamanhos em potências de 2 */
-#define PAD(n) (((n & (n - 1)) == 0) ? (n+1) : n)
+#define PAD(n) ((((n) & ((n) - 1)) == 0) ? ((n)+1) : (n))
 
 
 /*  Retorna tempo em milisegundos
