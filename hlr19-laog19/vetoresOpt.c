1733553a8614988c222342767aaa524156ba3c8e
Henrique Rieger <henriquerieger2001@gmail.com>
2021-12-16 19:27:12 -0300

    [change] - aumenta tamanho das variaveis em +1

--- hlr19-laog19/vetoresOpt.c
+++ hlr19-laog19/vetoresOpt.c
@@ -43,7 +43,7 @@ double * vetorAproximacoes(int n) {
 char ** vetorVariaveis(int n) {
     char ** variaveis = malloc(PAD(n)*sizeof(char *));
     for (int i = 0; i < n; i++) {
-        variaveis[i] = malloc(11*sizeof(char));
+        variaveis[i] = malloc(12*sizeof(char));
         sprintf(variaveis[i], "x%d", i+1);
     }
 
